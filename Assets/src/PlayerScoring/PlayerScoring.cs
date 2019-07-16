184483af2a6dd1a9a901badce6a064ada8813b6f
John P <jpigottmail@gmail.com>
2019-07-15 21:46:20 -0400

    Added the player scoring module with a single players score and a method to get and update it

---
+++ Assets/src/PlayerScoring/PlayerScoring.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoring : MonoBehaviour
+{
+    //Will probably want to expand this to an array and then object as the features are but in during later increments
+    private int playerScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetPlayerScores(){
+        return playerScore;
+    }
+
+    public void UpdatePlayerScore(int scoreAdjustment){
+        playerScore += scoreAdjustment;
+    }
+}
