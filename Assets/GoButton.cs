9eea2351e746d6defc3a56d058c6c32052dda935
Ben Gianni <bgianni@bens-mbp.lan>
2019-07-15 22:07:24 -0400

    added some baseline functionality to supply categories, questions and answers.

---
+++ Assets/GoButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoButton : MonoBehaviour
+{
+    public void onSubmit()
+    {
+        var q = GameObject.Find("Canvas/SkeletalMenu/Question").GetComponent<Text>();
+        var ans = GameObject.Find("Canvas/SkeletalMenu/input").GetComponent<InputField>();
+        TextAsset answers = (TextAsset)Resources.Load("answers");
+        var content = answers.text;
+        if (ans.text.ToString().Trim() == content.ToString())
+        {
+            q.text = "Correct!";
+        }
+        else
+        {
+            q.text = "Incorrect!";
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/src/GameEngine/GameEngine.cs
+++ Assets/src/GameEngine/GameEngine.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class GameEngine : MonoBehaviour
 {
-    public PlayerScoring playerScoring; 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Game Engine: Player score is " + playerScoring.GetPlayerScores());
-        Debug.Log("Game Engine: Adding 200 points to the player");
-        playerScoring.UpdatePlayerScore(200);
-        Debug.Log("Game Engine: Player score is " + playerScoring.GetPlayerScores());
-        Debug.Log("Game Engine: Removing 400 points from the player");
-        playerScoring.UpdatePlayerScore(-400);
-        Debug.Log("Game Engine: Player score is " + playerScoring.GetPlayerScores());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void UIRequestMessage()
