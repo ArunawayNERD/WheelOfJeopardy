2824a5e9ce80fb6c2cfcf81c28b936286d670db5
Ben Gianni <bgianni576@gmail.com>
2019-07-26 17:10:13 -0400

    added categories to the wheel from a csv

---
+++ Assets/Resources/Question.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question
+{
+	public string title;
+	public string answer;
+	public string points;
+
+	public Question(string title, string answer, string points) {
+		this.title = title;
+		this.answer = answer;
+		this.points = points;
+	}
+}
--- Assets/src/GameEngine/GameEngine.cs
+++ Assets/src/GameEngine/GameEngine.cs
@@ -10,9 +10,9 @@ public class GameEngine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Game Engine: Adding 200 points to the player");
+        //Debug.Log("Game Engine: Adding 200 points to the player");
         playerScoring.UpdatePlayerScore(200);
-        Debug.Log("Game Engine: Removing 400 points from the player");
+        //Debug.Log("Game Engine: Removing 400 points from the player");
         playerScoring.UpdatePlayerScore(-400);
     }
 
@@ -24,20 +24,20 @@ public class GameEngine : MonoBehaviour
 
     public void UIRequestMessage()
     {
-        Debug.Log("Game Engine: Got your message UI");
+        //Debug.Log("Game Engine: Got your message UI");
     }
 
     public void UIRequestPlayerScore()
     {
-        Debug.Log("Game Engine: Got your message, getting player score now.");
+        //Debug.Log("Game Engine: Got your message, getting player score now.");
         score = playerScoring.GetPlayerScores();
-        Debug.Log("Game Engine: Received player score: "+ score);
+        //Debug.Log("Game Engine: Received player score: "+ score);
     }
 
     public void UIRequestQuestion()
     {
-        Debug.Log("Game Engine: Got your message, getting a question");
+        //Debug.Log("Game Engine: Got your message, getting a question");
         string[] questionAnswer = questionStore.getQuestion("Math", 200);
-        Debug.Log("Game Engine: The question is \"" + questionAnswer[0] + "\" with answer \"" + questionAnswer[1] + "\"");
+        //Debug.Log("Game Engine: The question is \"" + questionAnswer[0] + "\" with answer \"" + questionAnswer[1] + "\"");
     }
 }
