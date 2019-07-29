0d086be71011e6a8eceae6b5187eec3f20a11562
lmasz <lisamaszkiewicz@gmail.com>
2019-07-29 19:04:19 -0400

    Added bank end for changing player's score. Does not connect to UI or game engine yet.

--- Assets/src/PlayerScoring/PlayerScoring.cs
+++ Assets/src/PlayerScoring/PlayerScoring.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerScoring : MonoBehaviour
 {
-    //Will probably want to expand this to an array and then object as the features are but in during later increments
-    private int playerScore = 0;
+    
+    static int MAX_PLAYERS = 2;
+    int[] playerScores; 
+    
+    // Will probably want to expand this to an array and then object as the features are but in during later increments
+    // for now we will assume they provide the player number, maybe in future the name is a good ID.
+    // should player's number of free turns should be stored here maybe?
 
     // Start is called before the first frame update
     void Start()
     {
-
+        int[] playerScores = new int[MAX_PLAYERS];
     }
 
     // Update is called once per frame
@@ -19,14 +24,26 @@ public class PlayerScoring : MonoBehaviour
         
     }
 
-    public int GetPlayerScores()
+    public int GetPlayerScore(int playerNum)
     {
         //Debug.Log("Player Scoring: Got your message, sending you the player score now.");
-        return playerScore;
+        return playerScores[playerNum];
     }
 
-    public void UpdatePlayerScore(int scoreAdjustment){
-        playerScore += scoreAdjustment;
+    public void UpdatePlayerScore(int playerNum, int scoreAdjustment){
         //Debug.Log("Player Scoring: Player score is " + this.GetPlayerScores());
+        playerScores[playerNum] += scoreAdjustment;
     }
+
+    public int DoublePlayerScore(int playerNum)
+    {
+        playerScores[playerNum] *= 2;
+        return playerScores[playerNum];
+    }
+
+    public void BankruptPlayer(int playerNum)
+    {
+        playerScores[playerNum] = 0;
+    }
+
 }
