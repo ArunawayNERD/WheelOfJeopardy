c667f80a357209d53ccd7bdd47ceced2b4333a70
John P <jpigottmail@gmail.com>
2019-07-29 23:35:22 -0400

    reworked/finished the parsing logic so that the questoion array is populated

--- Assets/src/QuestionStore/QuestionStore.cs
+++ Assets/src/QuestionStore/QuestionStore.cs
@@ -7,13 +7,19 @@ using System.Linq;
 public class QuestionStore : MonoBehaviour
 {   
 	public Text sector, sector1, sector2, sector3, sector4, sector5, sector6, sector7, sector8, sector9, sector10, sector11; 
-    private Dictionary<string, Dictionary<int, string[]>> questions;
+    private Dictionary<string, Dictionary<int, Question>> questions;
 
-    void Awake(){
+    private const int CATEGORY_INDEX = 0;
+    private const int QUESTION_INDEX = 1;
+    private const int ANSWER_INDEX = 2;
+    private const int SCORE_INDEX = 3;
+
+    void Awake()
+    {
     	Dictionary<int, string[]> skeletalQuestions = new Dictionary<int, string[]>();
-    	questions = new Dictionary<string, Dictionary<int, string[]>>();
-    	List<Question> qStore = new List<Question>();
-    	List<Category> catStore = new List<Category>();
+    	questions = new Dictionary<string, Dictionary<int, Question>>();
+    	//List<Question> qStore = new List<Question>();
+    	//List<Category> catStore = new List<Category>();
     	List<Text> sectors = new List<Text>();
     	sectors.Add(sector);
     	sectors.Add(sector1);
@@ -31,48 +37,65 @@ public class QuestionStore : MonoBehaviour
 		
     	TextAsset userInput = Resources.Load<TextAsset>("QuestionData");
     	string[] data = userInput.text.Split('\n');
-    	//Debug.Log(userInput.text);
-    	
-    	for (int row = 1; row < data.Length - 1; row++) {
-    		if (row == 1) {		
-    			//category
-    			cats = data[row].Split(',');
-    			for (int j = 0; j < cats.Length; j++) {
-    				sectors[j].text = cats[j];
-    				//catStore.Add(new Category(cats[j]));
-    				//Debug.Log(cats[j]);
-    			} 
-    			
-    			//Debug.Log(data[row]);   			
-    		}
-    	}
-    	sectors[6].text = "Lose turn";
-    	sectors[7].text = "Free turn";
-    	sectors[8].text = "Bankrupt";
-    	sectors[9].text = "Player's choice";
-    	sectors[10].text = "Opponent's choice";
-    	sectors[11].text = "Double your Score";
-    	
-    	//Debug.Log(catStore[0].getQuestion().Count);
-        
-        
-        string[] question1 = {"2+2", "4"};
-        string[] question2 = {"2+2", "4"};
-        string[] question3 = {"2+2", "4"};
-        string[] question4 = {"2+2", "4"};
-        string[] question5 = {"2+2", "4"};
+        //Debug.Log(userInput.text);
 
-        skeletalQuestions.Add(200, question1);
-        skeletalQuestions.Add(400, question2);
-        skeletalQuestions.Add(600, question3);
-        skeletalQuestions.Add(800, question4);
-        skeletalQuestions.Add(1000, question5);
+        int catCount = 0;
+        for(int i=0; i < data.Length; i++)
+        {
+            if(i > 0) //row 0 is header info
+            {
+                string[] guestionData = data[i].Split(',');
 
-        questions.Add("Math", skeletalQuestions);
-		
+                string category = guestionData[CATEGORY_INDEX];
 
+                //We havnt run into this key yet so add its nested dict and update the sectors
+                if (!questions.ContainsKey(category))
+                {
+                    //sectors[catCount].text = category;
+                    //catCount++;
 
+                    Debug.Log(category);
+                    questions.Add(category, new Dictionary<int, Question>());
+                }
 
+                int score = int.Parse(guestionData[SCORE_INDEX]);
+
+            
+                string questionText = guestionData[QUESTION_INDEX];
+                string answer = guestionData[ANSWER_INDEX];
+
+                //Debug.Log(score);
+                //Debug.Log(questionText);
+                //Debug.Log(answer);
+
+                questions[category].Add(score, new Question(questionText, answer, score, category));
+                //Debug.Log(questions.ContainsKey(category));
+                //Debug.Log(questions[category].ContainsKey(score));
+            }
+
+        }
+
+        //for (int row = 1; row < data.Length - 1; row++) {
+        //	if (row == 1) {		
+        //		//category
+        //		cats = data[row].Split(',');
+        //		for (int j = 0; j < cats.Length; j++) {
+        //			sectors[j].text = cats[j];
+        //			//catStore.Add(new Category(cats[j]));
+        //			//Debug.Log(cats[j]);
+        //		} 
+
+        //		//Debug.Log(data[row]);   			
+        //	}
+        //}
+        sectors[6].text = "Lose turn";
+        sectors[7].text = "Free turn";
+        sectors[8].text = "Bankrupt";
+        sectors[9].text = "Player's choice";
+        sectors[10].text = "Opponent's choice";
+        sectors[11].text = "Double your Score";
+
+        //Debug.Log(catStore[0].getQuestion().Count);
     }
 
     // Start is called before the first frame update
@@ -91,7 +114,7 @@ public class QuestionStore : MonoBehaviour
         return this.questions.Keys.ToArray();
     }
 
-    public string[] getQuestion(string category, int pointValue){
+    public Question getQuestion(string category, int pointValue){
         Debug.Log("Question Store: Getting question " + category + " for " + pointValue);
         return  this.questions[category][pointValue];
     }
