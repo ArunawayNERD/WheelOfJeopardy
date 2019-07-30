ebdd210e0bf95d3631b9443577e971d4e784489d
John P <jpigottmail@gmail.com>
2019-07-29 21:52:04 -0400

    Added the QuestionMenu component and start of script

---
+++ Assets/src/UI/QuestionMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionMenu : MonoBehaviour
+{
+    public GameObject menuGraphics;
+
+    public Text title;
+	public Text displayText;
+
+	public Button showAnswer;
+    public Button correct;
+    public Button incorrect;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.ResetMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ToggleVisible(bool show) {
+		this.menuGraphics.SetActive(show);
+	}
+
+	private void ResetMenu() {
+        this.displayText.text = "Question";
+
+        this.showAnswer.gameObject.SetActive(true);
+        this.correct.gameObject.SetActive(false);
+        this.incorrect.gameObject.SetActive(false);
+
+
+    }
+
+    private void SwitchToAnswerMode()
+    {
+        this.displayText.text = "Question";
+
+        this.showAnswer.gameObject.SetActive(true);
+        this.correct.gameObject.SetActive(false);
+        this.incorrect.gameObject.SetActive(false);
+    }
+
+
+}
