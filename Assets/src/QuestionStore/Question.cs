94b1fc2b7b5f85dd7ab304b23e9a9d0ccc887ded
John P <jpigottmail@gmail.com>
2019-07-29 23:34:13 -0400

    Renamed some things to make the question class match the class diagram

--- Assets/src/QuestionStore/Question.cs
+++ Assets/src/QuestionStore/Question.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Question
 {
-	public string title;
+	public string question;
 	public string answer;
-	public string points;
+	public int points;
+    public string category;
 
-	public Question(string title, string answer, string points) {
-		this.title = title;
+	public Question(string question, string answer, int points, string category) {
+		this.question = question;
 		this.answer = answer;
 		this.points = points;
+        this.category = category;
 	}
 }
