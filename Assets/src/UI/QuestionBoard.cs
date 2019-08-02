5f4cb8cbd473ac6c11d4a273c7160bd04d4e631e
John P <jpigottmail@gmail.com>
2019-08-01 22:57:25 -0400

    Added the board game object

---
+++ Assets/src/UI/QuestionBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionBoard : MonoBehaviour
+{
+	public GameEngine gameEngine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //Get the question data
+	  
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
