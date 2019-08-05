a67db05cc6f3817afe975c5dda63871ab07b7d0d
Adam Abate <abateadam6@gmail.com>
2019-08-04 20:31:41 -0400

    Add Player class

---
+++ Assets/src/PlayerScoring/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.src.PlayerScoring
+{
+    class Player
+    {
+        private string name;
+        private int roundOneScore;
+        private int roundTwoScore;
+        private int tokens;
+
+        public string Name { get => name; set => name = value; }
+        public int RoundOneScore { get => roundOneScore; set => roundOneScore = value; }
+        public int RoundTwoScore { get => roundTwoScore; set => roundTwoScore = value; }
+        public int Tokens { get => tokens; set => tokens = value; }
+
+        public Player()
+        {
+            Console.WriteLine("Yo waddUp homie Im a pl8r");
+        }
+
+        public void UseToken()
+        {
+
+        }
+
+        public void AddToken()
+        {
+
+        }
+    }
+}
